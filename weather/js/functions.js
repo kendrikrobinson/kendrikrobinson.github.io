a4da58b42c302c95dff357827b2c1811406d25cc
Kendrik Robinson <kendrikrobinson@Kendriks-MacBook-Pro-2.local>
2019-03-09 16:52:38 -0700

    Updated

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -152,16 +152,61 @@ function getCondition(statement) {
     } else if (statement == 'raining' ||
         statement == 'pouring' ||
         statement == 'precipitation' ||
-        statement == 'wet') {
+        statement == 'wet' ||
+        statement == 'rain') {
         condition = 'rain'
         return condition;
-    } else{
+    } else {
         condition = 'clear';
         return condition;
     }
 }
-    //change the summary image function
-    function changeSummaryImage(condition){
-        document.getElementById('rain').setAttribute("class", condition);
-        return 0;
+//change the summary image function
+function changeSummaryImage(condition) {
+    document.getElementById('rain').setAttribute("class", condition);
+    document.getElementById('summary').setAttribute("class", condition);
+    document.getElementById('weatherType').innerHTML = condition;
+    return 0;
+
+}
+
+
+
+// Convert to 12 hour hourly temp
+function format_time(hour) {
+    if (hour > 23) {
+        hour -= 24;
     }
+    let amPM = (hour > 11) ? "pm" : "am";
+    if (hour > 12) {
+        hour -= 12;
+    }
+    if (hour == 0) {
+        hour = "12";
+    }
+    return hour + amPM;
+}
+
+
+
+// Build the hourly temp list
+function buildHourlyData(nextHour, hourlyTemps) {
+    // Data comes from a JavaScript object of hourly temp name - value pairs
+    // Next hour should have a value between 0-23
+    // The hourlyTemps variable holds an array of temperatures
+    // Line 8 builds a list item showing the time for the next hour 
+    // and then the first element (value in index 0) from the hourly temps array
+    let hourlyListItems = '<li>' + format_time(nextHour) + ':' + hourlyTemps[0] + '&deg;F</li>';
+    // Build the rest in a loop
+    for (let i = 1, x = hourlyTemps.length; i < x; i++) {
+        hourlyListItems += '<li>' + format_time(nextHour + i) + ': ' + hourlyTemps[i] + '&deg;F</li>';
+    }
+    console.log('HourlyList is: ' + hourlyListItems);
+    return hourlyListItems;
+}
+
+// Get next hour based on time
+let date = new Date();
+let nextHour = date.getHours() + 1;
+
+
---
+++ weather/js/local.js
@@ -0,0 +1,112 @@
+
+/* *************************************
+*  Weather Site JavaScript Functions
+************************************* */
+
+
+// DOM structures from webpage
+"use strict"
+let pageNav = document.getElementById('page-nav');
+let statusContainer = document.getElementById('status');
+let contentContainer = document.getElementById('page-content');
+
+let weatherURL = "/weather/js/weather.json";
+fetchData(weatherURL);
+function fetchData(weatherURL){
+    let cityName = 'Greenville'; // The data we want from the weather.json file
+    fetch(weatherURL)
+    .then(function(response) {
+        if(response.ok){
+            return response.json();
+        }
+        throw new ERROR('Network response was not OK.');
+    })
+    .then(function(data){
+        // Check the data object that was retrieved
+        console.log(data);
+        // data is the full JavaScript object, but we only want the greenville part
+        // shorten the variable and focus only on the data we want to reduce typing
+        let g = data[cityName];
+
+    // ************ Get the content ******************************
+
+    // Get the location data
+    let locName = g.City;
+    let locState = g.State;
+    // Put them together
+    let fullName = locName+', '+locState;
+    // See if it worked
+    console.log('fullName is: '+fullName);
+
+    // Get the temperature data
+    let curTemp = g.Temp;
+    let high = g.High;
+    let low = g.Low;
+    // See if it worked 
+    console.log("Temp info is: " + curTemp + " " + high + " " + low);
+
+    // Get the wind data 
+    let windSpeed = g.Wind;
+    let gusts = g.Gusts;
+    let windDirection = g.Direction;
+    // Test
+    console.log("Wind is " + wind + ", Direction is " + direction + ", Gusts are " + gusts);
+
+    // Get the current conditions
+    let precipitation = g.Precip;
+    let weather = g.Summary;
+    // Test
+    console.log("It is currently " + weather + " and the precipitation is " + precipitation);
+
+    // Get the hourly data 
+    let hourly = g.Hourly;
+    // Test
+    console.log("The hourly data is " + hourly);
+
+    // ************ Display the content ******************************
+    // Set the title with the location name at the first
+    // Gets the title element so it can be worked with
+    let pageTitle = document.getElementById('page-title');
+    // Create a text node containing the full name 
+    let fullNameNode = document.createTextNode(fullName);
+    // inserts the fullName value before any other content that might exist
+    pageTitle.insertBefore(fullNameNode, pageTitle.childNodes[0]);
+    // When this is done the title should look something like this:
+    // Greenville, SC | The Weather Site
+
+    // Set the Location information
+    // Get the h1 to display the city location
+    let contentHeading = document.getElementById('locName');
+    contentHeading.innerHTML = fullName;
+    // The h1 in main h1 should now say "Greenville, SC"
+    // Set the temperature information
+    document.getElementById("curTemp").innerHTML = curTemp;
+    document.getElementById("high").innerHTML = high + "&deg;";
+    document.getElementById("low").innerHTML = low + "&deg;";
+
+    // Set the wind information
+    document.getElementById("mph").innerHTML = windSpeed + " mph";
+    document.getElementById("gusts").innerHTML = gusts + " mph";
+    document.getElementById("direction").innerHTML = windDirection;
+
+    // Set the current conditions information
+    document.getElementById("current").innerHTML = weather;
+    console.log(weather);
+
+    // Set the hourly temperature information
+    var ul = document.getElementById("hourlyLI");
+    var items = ul.getElementsByTagName("li");
+    for (var i = 0; i < items.length; ++i){
+        items[i].innerHTML = hourly[i];
+    }
+
+    // Change the status of the containers
+    pageContent.setAttribute('class', ''); // removes the hide class
+    statusMessage.setAttribute('class', 'hide'); // hides the status container
+  })
+  .catch(function(error){
+  console.log('There was a fetch problem: ', error.message);
+  statusMessage.innerHTML ='Sorry, the data could not be processed.';
+})
+}
+
